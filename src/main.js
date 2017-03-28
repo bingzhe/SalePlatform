6bb4eb9d104492ff3931807e3150dbb831891bd3
ranzhouli <874177815@qq.com>
2017-03-28 17:05:01 +0800

    layout

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  router,
+  template: '<Layout/>',
+  components: { Layout }
 })
