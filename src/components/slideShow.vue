82cf33c5afecdfef4e81907e8bdf9a045c83ec04
bingzhe <874177815@qq.com>
2017-05-10 20:46:04 +0800

    vue slide component

--- src/components/slideShow.vue
+++ src/components/slideShow.vue
@@ -1,17 +1,22 @@
 <template>
-    <div class="slide-show">
+    <div class="slide-show" @mouseover="clearInv" @mouseout="runInv">
         <div class="slide-img">
             <a :href="slides[nowIndex].href">
-                <img :src="slides[nowIndex].src">
+                <transition name="slide-trans">
+                    <img v-if="isShow" :src="slides[nowIndex].src">
+                </transition>
+                <transition name="slide-trans-old">
+                    <img v-if="!isShow" :src="slides[nowIndex].src">
+                </transition>
             </a>
         </div>
         <h2>{{ slides[nowIndex].title }}</h2>
         <ul class="slide-pages">
-            <li>&lt;</li>
+            <li @click="goto(prevIndex)">&lt;</li>
             <li v-for="(item, index) in slides" @click="goto(index)">
-                <a>{{ index + 1 }}</a>
+                <a :class="{on: index === nowIndex}">{{ index + 1 }}</a>
             </li>
-            <li>&gt;</li>
+            <li @click="goto(nextIndex)">&gt;</li>
         </ul>
     </div>
 </template>
@@ -21,17 +26,53 @@
             slides: {
                 type: Array,
                 default: []
+            },
+            inv: {
+                type: Number,
+                default: 1000
             }
         },
         data () {
             return {
-                nowIndex: 2
+                nowIndex: 0
+            }
+        },
+        computed: {
+            prevIndex () {
+                if(this.nowIndex === 0){
+                    return this.slides.length - 1
+                } else {
+                    return this.nowIndex - 1
+                }
+            },
+            nextIndex () {
+                if(this.nowIndex === this.slides.length - 1){
+                    return 0
+                } else {
+                    return this.nowIndex + 1
+                }
             }
         },
         methods: {
             goto (index) {
-                this.nowIndex = index
+                this.isShow = false
+                setTimeout(() => {
+                    this.isShow = true
+                    this.nowIndex = index
+                }, 10)
+            },
+            runInv () {
+                this.invId = setInterval(() => {
+                    this.goto(this.nextIndex)
+                }, this.inv)
+            },
+            clearInv () {
+                clearInterval(this.invId)
             }
+        },
+        //生命周期，挂载完成，模板中的html页面渲染到html页面中，执行自动滚动
+        mounted () {
+            this.runInv()
         }
     }
 </script>
