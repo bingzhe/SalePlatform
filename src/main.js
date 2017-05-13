045944fa65192fcbb81ee021a3c633930ce92672
ranzhouli <874177815@qq.com>
2017-05-13 08:57:52 +0800

    detail

--- src/main.js
+++ src/main.js
@@ -5,24 +5,51 @@ import Layout from './components/layout'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import IndexPage from './pages/index'
+import DetailPage from './pages/detail'
+import DetailAnaPage from './pages/detail/analysis'
+import DetailCouPage from './pages/detail/count'
+import DetailForPage from './pages/detail/forecast'
+import DetailPubPage from './pages/detail/publish'
+
 Vue.use(VueRouter)
 Vue.use(VueResource)
 let router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: IndexPage
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            component: IndexPage
+        },
+        {
+            path: '/detail',
+            component: DetailPage,
+            //redirect: '/detail/analysis',
+            children: [{
+                    path: 'analysis',
+                    component: DetailAnaPage
+                },
+                {
+                    path: 'count',
+                    component: DetailCouPage
+                },
+                {
+                    path: 'forecast',
+                    component: DetailForPage
+                },
+                {
+                    path: 'publish',
+                    component: DetailPubPage
+                }
+            ]
+        }
+    ]
 })
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<Layout/>',
-  components: { Layout }
+    el: '#app',
+    router,
+    template: '<Layout/>',
+    components: { Layout }
 })
---
+++ src/pages/detail/analysis.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        111111111111111
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
+
